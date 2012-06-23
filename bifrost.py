6860377157b0bc683e486badace8a22260be28e3
Kenrick Turner <kenrickturner@gmail.com>
2012-06-24 00:27:43 +0100

    Updating AMI to use Tomcat7

--- bifrost.py
+++ bifrost.py
@@ -18,7 +18,7 @@ orgName = "uamuzibora" # Organisation that owns the repo
 awsKeyPair = "UamuziBora"
 # Do not change these!
 conn = None
-ami = "ami-0970757d" # The AMI ID of our base instance on EC2
+ami = "ami-03464277" # The AMI ID of our base instance on EC2
 dbRootPassword = "Out6Of7Africa42" # Password for root MySQL user on EC2 instance
 
 # Set our username as our GitHub username
